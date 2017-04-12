fc0159c0e0e73073fe65737de2ee27fa5ca78d47
hebisuke <hebizoro@gmail.com>
2017-04-12 22:43:13 +0700

    chuc nang product , details, phan trang

--- DoAn_ShopOnline/DoAn_ShopOnline/Controllers/ShopController.cs
+++ DoAn_ShopOnline/DoAn_ShopOnline/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DoAn_ShopOnline.Models.BUS;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,15 +11,17 @@ namespace DoAn_ShopOnline.Controllers
     public class ShopController : Controller
     {
         // GET: Shop
-        public ActionResult Index()
+        public ActionResult Index(int page = 1, int pagesize = 3)
         {
-            return View();
+            var db = ShopOnlineBUS.DanhSach().ToPagedList(page, pagesize);
+            return View(db);
         }
 
         // GET: Shop/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(String id)
         {
-            return View();
+            var db = ShopOnlineBUS.ChiTiet(id);
+            return View(db);
         }
 
         // GET: Shop/Create
---
+++ DoAn_ShopOnline/DoAn_ShopOnline/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,22 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_ShopOnline.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where TinhTrang = 0");
+        }
+        public static SanPham ChiTiet(String a)
+        {
+            var db = new ShopOnlineConnectionDB();
+             return db.SingleOrDefault<SanPham> ("select * from SanPham where MaSanPham = @0",a);
+        }
+    }
+}
