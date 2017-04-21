b80f0238bca8f9fda6e793f2ea9be840d64d210b
hebisuke <hebizoro@gmail.com>
2017-04-21 11:28:02 +0700

    create , edit ,delete Product Type

--- DoAn_ShopOnline/DoAn_ShopOnline/Areas/Admin/Controllers/LoaiSanPhamAdminController.cs
+++ DoAn_ShopOnline/DoAn_ShopOnline/Areas/Admin/Controllers/LoaiSanPhamAdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DoAn_ShopOnline.Models.BUS;
+using ShopOnlineConnection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,8 @@ namespace DoAn_ShopOnline.Areas.Admin.Controllers
         // GET: Admin/LoaiSanPhamAdmin
         public ActionResult Index()
         {
-            return View();
+            var db = LoaiSanPhamBUS.DanhSachAdmin();
+            return View(db);
         }
 
         // GET: Admin/LoaiSanPhamAdmin/Details/5
@@ -28,12 +31,12 @@ namespace DoAn_ShopOnline.Areas.Admin.Controllers
 
         // POST: Admin/LoaiSanPhamAdmin/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(LoaiSanPham lsp)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                LoaiSanPhamBUS.InsertLSP(lsp);
                 return RedirectToAction("Index");
             }
             catch
@@ -43,19 +46,21 @@ namespace DoAn_ShopOnline.Areas.Admin.Controllers
         }
 
         // GET: Admin/LoaiSanPhamAdmin/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(String id)
         {
-            return View();
+            // load db theo id
+            var db = LoaiSanPhamBUS.ChiTietAdmin(id);
+            return View(db);
         }
 
         // POST: Admin/LoaiSanPhamAdmin/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(String id, LoaiSanPham lsp)
         {
             try
             {
                 // TODO: Add update logic here
-
+                LoaiSanPhamBUS.EditLSP(id, lsp);
                 return RedirectToAction("Index");
             }
             catch
@@ -63,7 +68,29 @@ namespace DoAn_ShopOnline.Areas.Admin.Controllers
                 return View();
             }
         }
+        // GET: Admin/LoaiSanPhamAdmin/XoaTamThoi/5
+        public ActionResult XoaTamThoi(String id)
+        {
+            var db = LoaiSanPhamBUS.ChiTietAdmin(id);
+            return View(db);
+        }
 
+        // POST: Admin/LoaiSanPhamAdmin/XoaTamThoi/5
+        [HttpPost]
+        public ActionResult XoaTamThoi(String id, LoaiSanPham lsp)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                lsp.TinhTrang = "1";
+                LoaiSanPhamBUS.EditLSP(id, lsp);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
         // GET: Admin/LoaiSanPhamAdmin/Delete/5
         public ActionResult Delete(int id)
         {
--- DoAn_ShopOnline/DoAn_ShopOnline/Models/BUS/LoaiSanPhamBUS.cs
+++ DoAn_ShopOnline/DoAn_ShopOnline/Models/BUS/LoaiSanPhamBUS.cs
@@ -8,6 +8,7 @@ namespace DoAn_ShopOnline.Models.BUS
 {
     public class LoaiSanPhamBUS
     {
+        //------------ kHACH hANG
         public static IEnumerable<LoaiSanPham> DanhSach()
         {
             var db = new ShopOnlineConnectionDB();
@@ -18,5 +19,34 @@ namespace DoAn_ShopOnline.Models.BUS
             var db = new ShopOnlineConnectionDB();
             return db.Query<SanPham>("select * from SanPham where MaLoaiSanPham = '"+id+ "'");
         }
+
+
+        //--------------- ADMIN
+        public static IEnumerable<LoaiSanPham> DanhSachAdmin()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<LoaiSanPham>("select * from LoaiSanPham");
+        }
+        public static void InsertLSP( LoaiSanPham lsp)
+        {
+            var db = new ShopOnlineConnectionDB();
+            db.Insert(lsp);
+        }
+        public static LoaiSanPham ChiTietAdmin(String id)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.SingleOrDefault<LoaiSanPham>("select * from LoaiSanPham where MaLoaiSanPham = '" + id + "'");
+        }
+        public static void EditLSP(String id,LoaiSanPham lsp)
+        {
+            var db = new ShopOnlineConnectionDB();
+            db.Update(lsp,id);
+        }
+        public static void DeleteLSP(String id, LoaiSanPham lsp)
+        {
+            var db = new ShopOnlineConnectionDB();
+            db.Update(lsp, id);
+        }
+
     }
 }
